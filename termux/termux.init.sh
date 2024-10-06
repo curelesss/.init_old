85e517503a40a6a79fed5e928d48d6b4ec6ad6c9
cureless <cureless@gmail.com>
2024-10-06 11:45:04 +0800

    minor

--- termux/termux.init.sh
+++ termux/termux.init.sh
@@ -2,3 +2,15 @@
 
 termux-setup-storage
 termux-change-repo
+
+pkg update
+pkg upgrade
+
+pkg install python
+pip install wheel
+pkg install rust
+export CARGO_BUILD_TARGET=aarch64-linux-android
+apt install python-cryptography
+pip install ansible
+pkg install git
+
